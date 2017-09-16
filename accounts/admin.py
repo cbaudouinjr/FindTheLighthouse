e58187c9198aad8191a45812b22ef5b574bf9b34
cjb5326 <cjb5326@cs.rit.edu>
2017-09-16 16:15:44 -0400

    Registered accounts/models.py into accounts/admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from accounts.models import *
 from django.contrib.auth.admin import UserAdmin
 
+admin.site.register(Interest)
+admin.site.register(School)
+admin.site.register(ExtUser)
+admin.site.register(Event)
+admin.site.register(Attendee)
