03989f6247d9e05e8fc6cd8befa835fad9754a20
Trevor Seiders <txs6996@g.rit.edu>
2017-09-24 00:38:58 -0400

    Updates and edits to the registration page, along with its full conversion to a manual display.

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,16 +5,20 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 # Form used for registering new Users
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField
-    username = forms.CharField(max_length=50)
-    password1 = forms.CharField(widget=forms.PasswordInput())
-    password2 = forms.CharField(widget=forms.PasswordInput())
+    email = forms.EmailField(initial="someone@example.com",
+                             error_messages={'required': 'Please enter a valid email.',
+                                             'invalid': 'This email is invalid.'})
+    username = forms.CharField(max_length=50, initial="username", help_text="Must be unique.")
+    password1 = forms.CharField(widget=forms.PasswordInput(), initial="enter a password",
+                                label="Password")
+    password2 = forms.CharField(widget=forms.PasswordInput(), initial="enter the password again",
+                                label="Confirm Password")
     first_name = forms.CharField(max_length=50)
     last_name = forms.CharField(max_length=50)
     # This data must be parsed into individual interest entities from the
     #   comma-separated charField.
-    interest_input = forms.CharField(label="I like...", required=False)
-    school = forms.ModelChoiceField(label="I attend...", queryset=School.objects.all())
+    interest_input = forms.CharField(label="Interests", initial="I like...", required=False)
+    school = forms.ModelChoiceField(label="School", initial="I attend...", queryset=School.objects.all())
 
     def clean(self):
         clean_data = super(RegistrationForm, self).clean()
